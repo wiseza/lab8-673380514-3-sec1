d13bfe12874b75a4b50e129c4de259b441757f78
wiseza <wiphawi_ri@kkumail.com>
2026-09-07 21:39:05 +0700

    Update SeasonalSaleStrategy.java

--- demo/src/main/java/com/example/demo/strategy/SeasonalSaleStrategy.java
+++ demo/src/main/java/com/example/demo/strategy/SeasonalSaleStrategy.java
@@ -1,5 +1,9 @@
 package com.example.demo.strategy;
 
-public class SeasonalSaleStrategy {
-    
+public class SeasonalSaleStrategy implements DiscountStrategy {
+
+    @Override
+    public double calculateDiscount(double price) {
+        return price * 0.80;
+    }
 }

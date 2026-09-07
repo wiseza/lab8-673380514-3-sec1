86ebd8a823cb84027c6adb8d25dad77b66b79be6
wiseza <wiphawi_ri@kkumail.com>
2026-09-07 19:00:08 +0700

    Create ProductDetail.java

---
+++ demo/src/main/java/com/example/demo/model/ProductDetail.java
@@ -0,0 +1,76 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_details")
+public class ProductDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String warranty;
+    private Double weight;
+    private String dimensions;
+    private String manufacturedCountry;
+
+    @OneToOne(mappedBy = "detail")
+    private Product product;
+
+    public ProductDetail() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(String warranty) {
+        this.warranty = warranty;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getManufacturedCountry() {
+        return manufacturedCountry;
+    }
+
+    public void setManufacturedCountry(String manufacturedCountry) {
+        this.manufacturedCountry = manufacturedCountry;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+}

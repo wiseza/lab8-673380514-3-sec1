d1f279f4c6403bb362427ebf8fbdaf702647b32c
wiseza <wiphawi_ri@kkumail.com>
2026-09-07 21:39:30 +0700

    Update ProductDetailRepository.java

--- demo/src/main/java/com/example/demo/repository/ProductDetailRepository.java
+++ demo/src/main/java/com/example/demo/repository/ProductDetailRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public class ProductDetailRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.ProductDetail;
+
+public interface ProductDetailRepository extends JpaRepository<ProductDetail, Long> {
+
 }

89e7d4527fb2f7d604d159ecad20f55f4e626983
wiseza <wiphawi_ri@kkumail.com>
2026-09-07 21:39:08 +0700

    Update NoDiscountStrategy.java

--- demo/src/main/java/com/example/demo/strategy/NoDiscountStrategy.java
+++ demo/src/main/java/com/example/demo/strategy/NoDiscountStrategy.java
@@ -1,5 +1,9 @@
 package com.example.demo.strategy;
 
-public class NoDiscountStrategy {
-    
+public class NoDiscountStrategy implements DiscountStrategy {
+
+    @Override
+    public double calculateDiscount(double price) {
+        return price;
+    }
 }

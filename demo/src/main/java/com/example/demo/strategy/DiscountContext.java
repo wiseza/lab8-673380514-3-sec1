221578abcaf08a294ec7a71b7ae571623ee45019
wiseza <wiphawi_ri@kkumail.com>
2026-09-07 21:39:16 +0700

    Update DiscountContext.java

--- demo/src/main/java/com/example/demo/strategy/DiscountContext.java
+++ demo/src/main/java/com/example/demo/strategy/DiscountContext.java
@@ -1,5 +1,14 @@
 package com.example.demo.strategy;
 
 public class DiscountContext {
-    
+
+    private DiscountStrategy strategy;
+
+    public DiscountContext(DiscountStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double calculatePrice(double price) {
+        return strategy.calculateDiscount(price);
+    }
 }
